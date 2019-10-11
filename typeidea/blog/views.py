136030cb638a49d3712c284f67bcbcaf8e6b12d0
zhaojianwei <zhaojianwei@rouchi.com>
2019-10-11 15:29:03 +0800

    view 展示基础信息

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Tag, Post
+
 
 def post_list(request, category_id=None, tag_id=None):
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            posts_list =[]
+        else:
+            posts_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    else:
+        posts_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+        if category_id:
+            posts_list = posts_list.filter(category_id=category_id)
 
     context = {
-        'name': 'post_list'
+        'post_list': posts_list
     }
     return render(request, 'blog/list.html', context=context)
 
 
 def post_detail(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
     context = {
-        'name': 'post_detail'
+        'post': post
     }
     return render(request, 'blog/detail.html', context=context)
