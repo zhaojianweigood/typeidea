9dca77fd0dac41af66649a3d61dd530e00c7d857
zhaojianwei <zhaojianwei@rouchi.com>
2019-10-11 15:10:42 +0800

    添加基本url-vim-template访问逻辑

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def post_list(request, category_id=None, tag_id=None):
+
+    context = {
+        'name': 'post_list'
+    }
+    return render(request, 'blog/list.html', context=context)
+
+
+def post_detail(request, post_id):
+    context = {
+        'name': 'post_detail'
+    }
+    return render(request, 'blog/detail.html', context=context)
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from .custom_site import custom_site
+
+from blog.views import post_list, post_detail
+from config.views import links
+from typeidea.custom_site import custom_site
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^super_admin/', custom_site.urls)
+    url(r'^super_admin/', custom_site.urls),
+
+    url(r'^$', post_list),
+    url(r'^category/(?P<category_id>\d+)/$', post_list),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
+    url(r'^post/(?P<post_id>\d+)/$', post_detail),
+    url(r'^links/$', links),
 ]
